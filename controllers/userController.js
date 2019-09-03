5135bd0c7fe3e88f762c0413fb7e8840253b7a0d
Mathias Kastrup <noeka@Mathiass-MBP.users.kollegie.dk>
2019-09-04 01:45:50 +0200

    Update dependencies & MongoDB methods.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,16 @@
-const User = require('../models/User');
-const config = require('config');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/User");
+const config = require("config");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-
   signup: (req, res, next) => {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!emailRegex.test(req.body.email)) {
       res.status(400).json({ error: "The entered email is not valid!" });
     } else {
-      User.count({ email: req.body.email }, (err, count) => {
+      User.countDocuments({ email: req.body.email }, (err, count) => {
         if (count > 0) {
           res.status(409).json({ error: "Email already exists!" });
         } else {
@@ -20,25 +19,28 @@ module.exports = {
               username: req.body.username,
               email: req.body.email,
               password: hash
-            }).then((user) => {
-              token = jwt.sign({ user: user.id }, config.Salt, { expiresIn: '1h' });
-
-              res.status(201).json({
-                message: "User successfully created!", 
-                user: { 
-                  username: user.username, 
-                  email: user.email,
-                  token: token
-                } 
-              })
-            }).catch((err) => {
-              res.status(500).json({ error: err.message })
             })
+              .then(user => {
+                token = jwt.sign({ user: user.id }, config.Salt, {
+                  expiresIn: "1h"
+                });
+
+                res.status(201).json({
+                  message: "User successfully created!",
+                  user: {
+                    username: user.username,
+                    email: user.email,
+                    token: token
+                  }
+                });
+              })
+              .catch(err => {
+                res.status(500).json({ error: err.message });
+              });
           });
         }
-      })
+      });
     }
-
   },
 
   signin: (req, res, next) => {
@@ -47,23 +49,24 @@ module.exports = {
         res.status(401).json({ error: "Unauthorized" });
       } else {
         bcrypt.compare(req.body.password, user.password, function(err, result) {
-          if(result) {
-            token = jwt.sign({ user: user.id }, config.Salt, { expiresIn: '1h' });
+          if (result) {
+            token = jwt.sign({ user: user.id }, config.Salt, {
+              expiresIn: "1h"
+            });
 
             res.status(201).json({
-              message: "User " + user.username + " logged in!", 
-              user: { 
-                username: user.username, 
+              message: "User " + user.username + " logged in!",
+              user: {
+                username: user.username,
                 email: user.email,
                 token: token
-              } 
-            })
+              }
+            });
           } else {
             res.status(401).json({ error: "Unauthorized" });
-          } 
+          }
         });
       }
     });
   }
-
-}
+};
--- test/integration/users.js
+++ test/integration/users.js
@@ -1,97 +1,118 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../../app');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../../app");
 const should = chai.should();
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 chai.use(chaiHttp);
 
-describe('AUTHENTICATION', () => {
+describe("AUTHENTICATION", () => {
   let token;
 
-  before((done) => {
-    User.remove({}, (err) => { 
-      done();         
+  before(done => {
+    User.deleteMany({}, err => {
+      done();
     });
   });
 
-  describe('GET /', () => {
-    it('It should NOT get page if no token provided', (done) => {
-      chai.request(app)
-        .get('/')
+  describe("GET /", () => {
+    it("It should NOT get page if no token provided", done => {
+      chai
+        .request(app)
+        .get("/")
         .end((err, res) => {
           res.should.have.status(403);
-          res.body.should.be.a('object');
-          res.body.should.have.property('error').eql('Token not provided.');
+          res.body.should.be.a("object");
+          res.body.should.have.property("error").eql("Token not provided.");
           done();
         });
     });
   });
 
-  describe('POST /auth/signup', () => {
-    it('It should create a user', (done) => {
-      chai.request(app)
-        .post('/auth/signup')
-        .send({ username: 'johndoe', email: 'john.doe@incorporated.com', password: 'lolololo' })
+  describe("POST /auth/signup", () => {
+    it("It should create a user", done => {
+      chai
+        .request(app)
+        .post("/auth/signup")
+        .send({
+          username: "johndoe",
+          email: "john.doe@incorporated.com",
+          password: "lolololo"
+        })
         .end((err, res) => {
           res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.have.property('message').eql('User successfully created!');
-          res.body.user.should.have.property('username');
-          res.body.user.should.have.property('email');
-          res.body.user.should.have.property('token');
+          res.body.should.be.a("object");
+          res.body.should.have
+            .property("message")
+            .eql("User successfully created!");
+          res.body.user.should.have.property("username");
+          res.body.user.should.have.property("email");
+          res.body.user.should.have.property("token");
           done();
         });
     });
-    it('Should fail if email is incorrect', (done) => {
-      chai.request(app)
-        .post('/auth/signup')
-        .send({ username: 'johndoe', email: 'hashshash.com', password: 'lolololo' })
+    it("Should fail if email is incorrect", done => {
+      chai
+        .request(app)
+        .post("/auth/signup")
+        .send({
+          username: "johndoe",
+          email: "hashshash.com",
+          password: "lolololo"
+        })
         .end((err, res) => {
           res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('error').eql('The entered email is not valid!');
+          res.body.should.be.a("object");
+          res.body.should.have
+            .property("error")
+            .eql("The entered email is not valid!");
           done();
         });
     });
   });
 
-  describe('POST /auth/signin', () => {
-    it('It should login a user', (done) => {
-      chai.request(app)
-        .post('/auth/signin')
-        .send({ email: 'john.doe@incorporated.com', password: 'lolololo' })
+  describe("POST /auth/signin", () => {
+    it("It should login a user", done => {
+      chai
+        .request(app)
+        .post("/auth/signin")
+        .send({ email: "john.doe@incorporated.com", password: "lolololo" })
         .end((err, res) => {
           res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.have.property('message').eql('User johndoe logged in!');
-          res.body.user.should.have.property('username');
-          res.body.user.should.have.property('email');
-          res.body.user.should.have.property('token');
+          res.body.should.be.a("object");
+          res.body.should.have
+            .property("message")
+            .eql("User johndoe logged in!");
+          res.body.user.should.have.property("username");
+          res.body.user.should.have.property("email");
+          res.body.user.should.have.property("token");
           done();
         });
     });
   });
 
-  describe('GET /', () => {
-    it('It should get page if user is logged in', (done) => {
-      chai.request(app)
-        .post('/auth/signin')
-        .send({ email: 'john.doe@incorporated.com', password: 'lolololo' })
+  describe("GET /", () => {
+    it("It should get page if user is logged in", done => {
+      chai
+        .request(app)
+        .post("/auth/signin")
+        .send({ email: "john.doe@incorporated.com", password: "lolololo" })
         .end((err, res) => {
-          chai.request(app)
-          .get('/')
-          .set('x-access-token', res.body.user.token)
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('message').eql('Welcome to the user protected area!');
-            done();
-          });
+          chai
+            .request(app)
+            .get("/")
+            .set("x-access-token", res.body.user.token)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a("object");
+              res.body.should.have
+                .property("message")
+                .eql("Welcome to the user protected area!");
+              done();
+            });
         });
     });
   });
-
 });
